f19512019f693b8b37f710b90d29c8aec4afb484
hesom <sommerhoff.hendrik@gmail.com>
2021-11-30 00:18:37 +0100

    Add docker build

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const he = require("he");
 const path = require("path");
 const axios = require("axios");
 const app = express();
-const port = 80;
+const port = 8080;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
